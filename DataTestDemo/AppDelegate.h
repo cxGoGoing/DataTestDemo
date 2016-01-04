d1c6b860e0ef012c4bbd9ee984dfc0a9dbac5fad
chengxun <chengxun08@163.com>
2016-01-04 22:20:22 +0800

    Initial Commit

---
+++ DataTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataTestDemo
+//
+//  Created by chengxun on 16/1/4.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
