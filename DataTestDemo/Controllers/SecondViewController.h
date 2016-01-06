f4a8314b4215b2005385544e21c1ddafe7192544
chengxun <chengxun08@163.com>
2016-01-06 10:23:29 +0800

    增加secondVcMethod

---
+++ DataTestDemo/Controllers/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  DataTestDemo
+//
+//  Created by chengxun on 16/1/6.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ DataTestDemo/Controllers/SecondViewController.m
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.m
+//  DataTestDemo
+//
+//  Created by chengxun on 16/1/6.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (void)secondeVCMethod{
+    NSLog(@"This is secondVc Method");
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
