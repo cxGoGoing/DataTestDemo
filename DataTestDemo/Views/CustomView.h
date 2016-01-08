8cf2f7fcfbc7df1b1079d933364fe6cfb5ead43b
chengxun <chengxun08@163.com>
2016-01-08 11:46:05 +0800

    增加一个纯洁的View

---
+++ DataTestDemo/Views/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  DataTestDemo
+//
+//  Created by chengxun on 16/1/8.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ DataTestDemo/Views/CustomView.m
@@ -0,0 +1,17 @@
+//
+//  CustomView.m
+//  DataTestDemo
+//
+//  Created by chengxun on 16/1/8.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+- (void)drawRect:(CGRect)rect{
+    [super drawRect:rect];
+}
+
+@end
